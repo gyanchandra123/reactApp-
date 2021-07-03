70e791cee63ab639fdea00e7d8ee8f1a634574a7
gyanchandra moirangthem <gyanchandra moirangthem>
2021-07-03 20:18:18 +0530

    28-simple-list

--- src/index.js
+++ src/index.js
@@ -2,91 +2,28 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-const firstBook = {
-  title: "Controlling MindsSpeechless",
-  img: "https://images-na.ssl-images-amazon.com/images/I/41JIh4KMHRL._AC_SX184_.jpg",
-  author: "Michael Knowles",
-};
-
-const secondBook = {
-  title: "The 48 Laws of Power",
-  img: "https://images-na.ssl-images-amazon.com/images/I/517s9eYVoHS._AC_SX184_.jpg",
-  author: "Robert Greene",
-};
+const names = ["aaaa", "bbbb", "cccc"];
 
 function Greeting() {
-  return (
-    <section className="booklist">
-      <Book
-        img={firstBook.img}
-        title={firstBook.title}
-        author={firstBook.author}
-      >
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Augue
-          lacus viverra vitae congue eu.
-        </p>
-      </Book>
-      <Book
-        img={secondBook.img}
-        title={secondBook.title}
-        author={secondBook.author}
-      />
-    </section>
-  );
+
+  const newNames = names.map(name=>{
+    return <h3>{name}</h3>
+  })
+
+  return( <section className="booklist">
+    {newNames}
+  </section>);
 }
 
-const Book = ({ img, title, author,children}) => { 
+const Book = ({ img, title, author }) => {
   // here the children: must maintain unique name
   return (
     <article className="book">
       <img src={img} alt="images" />
       <h4>{title}</h4>
       <h5>{author}</h5>
-      {children}
     </article>
   );
 };
 
 ReactDOM.render(<Greeting />, document.getElementById("root"));
-
-//wrong way:
-
-/* import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-
-const firstBook = {
-  title: "Controlling MindsSpeechless",
-  img: "https://images-na.ssl-images-amazon.com/images/I/41JIh4KMHRL._AC_SX184_.jpg",
-  author: "Michael Knowles",
-};
-
-const secondBook = {
-  title: "The 48 Laws of Power",
-  img: "https://images-na.ssl-images-amazon.com/images/I/517s9eYVoHS._AC_SX184_.jpg",
-  author: "Robert Greene",
-};
-
-function Greeting() {
-  return (
-    <section className="booklist">
-      <Book firstObj={firstBook} />
-      <Book secondObj={secondBook} />
-    </section>
-  );
-}
-
-const Book = (props) => {
-  const {firstObj, secondObj} = props;
-  return (
-    <article className="book">
-      <img src={firstObj.img || secondObj.img} alt="images" />
-      <h4>{firstObj.title || secondObj.title}</h4>
-      <h5>{firstObj.author || secondObj.author}</h5>
-    </article>
-  );
-};
-
-ReactDOM.render(<Greeting />, document.getElementById("root")); */
