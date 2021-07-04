2e038609ede99f9071a684b181be611dffc7eafa
gyanchandra moirangthem <gyanchandra moirangthem>
2021-07-04 08:17:47 +0530

    29 proper list

--- src/index.js
+++ src/index.js
@@ -2,28 +2,77 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-const names = ["aaaa", "bbbb", "cccc"];
+const books = [
+  {
+    title: "Controlling MindsSpeechless",
+    img: "https://images-na.ssl-images-amazon.com/images/I/41JIh4KMHRL._AC_SX184_.jpg",
+    author: "Michael Knowles",
+  },
+  {
+    title: "The 48 Laws of Power",
+    img: "https://images-na.ssl-images-amazon.com/images/I/517s9eYVoHS._AC_SX184_.jpg",
+    author: "Robert Greene",
+  },
+];
 
 function Greeting() {
-
-  const newNames = names.map(name=>{
-    return <h3>{name}</h3>
-  })
-
-  return( <section className="booklist">
-    {newNames}
-  </section>);
+  return (
+    <section className="booklist"> 
+     { books.map(book => <Book books={book}/>)} 
+    </section>
+  );
 }
 
-const Book = ({ img, title, author }) => {
-  // here the children: must maintain unique name
+const Book = (props) => { 
+  console.log(props)
+  const {img,title,author}= props.books;
   return (
     <article className="book">
       <img src={img} alt="images" />
       <h4>{title}</h4>
-      <h5>{author}</h5>
+      <h5>{author}</h5>   
     </article>
   );
 };
 
 ReactDOM.render(<Greeting />, document.getElementById("root"));
+
+//wrong way:
+
+/* import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+
+const firstBook = {
+  title: "Controlling MindsSpeechless",
+  img: "https://images-na.ssl-images-amazon.com/images/I/41JIh4KMHRL._AC_SX184_.jpg",
+  author: "Michael Knowles",
+};
+
+const secondBook = {
+  title: "The 48 Laws of Power",
+  img: "https://images-na.ssl-images-amazon.com/images/I/517s9eYVoHS._AC_SX184_.jpg",
+  author: "Robert Greene",
+};
+
+function Greeting() {
+  return (
+    <section className="booklist">
+      <Book firstObj={firstBook} />
+      <Book secondObj={secondBook} />
+    </section>
+  );
+}
+
+const Book = (props) => {
+  const {firstObj, secondObj} = props;
+  return (
+    <article className="book">
+      <img src={firstObj.img || secondObj.img} alt="images" />
+      <h4>{firstObj.title || secondObj.title}</h4>
+      <h5>{firstObj.author || secondObj.author}</h5>
+    </article>
+  );
+};
+
+ReactDOM.render(<Greeting />, document.getElementById("root")); */
